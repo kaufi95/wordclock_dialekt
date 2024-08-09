11c372e8592cfa66d7c2d7f1271085020517e82f
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2024-08-09 20:32:39 +0200

    Update wordclock_dialekt_WIFI-RTC.ino

--- wordclock_dialekt_WIFI-RTC/wordclock_dialekt_WIFI-RTC.ino
+++ wordclock_dialekt_WIFI-RTC/wordclock_dialekt_WIFI-RTC.ino
@@ -295,18 +295,18 @@ void refreshMatrix(bool settingsChanged) {
 void fillMatrix(time_t time) {
   if (config.language == "dialekt") {
     auto pixels = dialekt::timeToMatrix(time);
-    turnPixelsOn(pixels);
-    printMatrix(pixels);
+    drawPixelsOnMatrix(pixels);
+    printMatrixToSerial(pixels);
   }
   if (config.language == "deutsch") {
     auto pixels = deutsch::timeToMatrix(time);
-    turnPixelsOn(pixels);
-    printMatrix(pixels);
+    drawPixelsOnMatrix(pixels);
+    printMatrixToSerial(pixels);
   }
 }
 
 // turns the pixels from startIndex to endIndex of startIndex row on
-void turnPixelsOn(std::array<std::array<bool, 11>, 11> &pixels) {
+void drawPixelsOnMatrix(std::array<std::array<bool, 11>, 11> &pixels) {
   for (uint8_t i = 0; i < 11; i++) {
     for (uint8_t j = 0; j < 11; j++) {
       if (pixels[i][j]) {
@@ -316,7 +316,7 @@ void turnPixelsOn(std::array<std::array<bool, 11>, 11> &pixels) {
   }
 }
 
-void printMatrix(std::array<std::array<bool, 11>, 11> &pixels) {
+void printMatrixToSerial(std::array<std::array<bool, 11>, 11> &pixels) {
   for (uint8_t i = 0; i < 11; i++) {
     for (uint8_t j = 0; j < 11; j++) {
       Serial.print(pixels[i][j] ? "1" : "0");
